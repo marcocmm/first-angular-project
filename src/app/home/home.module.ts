6e047a4fd1334f7394bf6c9018c79ebae48d8438
Marco Cezar Moreira de Mattos <marco@MacBook-Pro-de-Marco.local>
2022-06-08 17:40:15 +0100

    Add home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
-import { HomeRoutingModule } from './home-routing.module';
-
+import { HomeRoutingModule } from './home-routing.module'
+import { HomeComponent } from './home.component'
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
+  exports: [HomeComponent],
 })
-export class HomeModule { }
+export class HomeModule {}

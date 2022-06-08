000d0a54dddbcdf45818f89bb9741dc8254d2f48
Marco Cezar Moreira de Mattos <marco@MacBook-Pro-de-Marco.local>
2022-06-08 18:29:36 +0100

    Add login component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { HomeRoutingModule } from './home-routing.module'
-import { HomeComponent } from './home.component'
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, LoginComponent],
   imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent],
 })
